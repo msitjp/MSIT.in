f4c9ad5d3518bffc5f4bf634092b22b396026cd4
vigzmv <vigzmv@outlook.com>
2017-03-20 23:27:57 +0530

    this is a commit

--- msit/settings.py
+++ msit/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tvp$q5j-#@@*e4ln788fvuv)x53e999cl5(vxs8lqkgjkt3xy5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['35.154.11.189', '139.59.61.186', 'localhost', '127.0.0.1', '[::1]']
 
@@ -64,24 +64,24 @@ WSGI_APPLICATION = 'msit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
  }
-"""
-DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-          'NAME': 'myproject',
-          'USER': 'myprojectuser',
-          'PASSWORD': 'password',
-          'HOST': 'localhost',
-          'PORT': '5432',
-      }
-}
+
+# DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#           'NAME': 'myproject',
+#           'USER': 'myprojectuser',
+#           'PASSWORD': 'password',
+#           'HOST': 'localhost',
+#           'PORT': '5432',
+#       }
+# }
 
 
 # Password validation
