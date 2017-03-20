bf0bbd0903563bec00df73642c2bbd65deacf0ba
sahilkhurana02 <sahil_khurana@outlook.com>
2017-03-20 21:47:36 +0530

    Css for faculty sorter

--- msit/settings.py
+++ msit/settings.py
@@ -64,14 +64,14 @@ WSGI_APPLICATION = 'msit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-# }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+ }
+"""
 DATABASES = {
       'default': {
           'ENGINE': 'django.db.backends.postgresql_psycopg2',
