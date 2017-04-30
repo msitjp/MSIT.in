6c9e3d27eebfa6b8368f773ff18d4682993d0ad2
Hiten <sharmahiten3@gmail.com>
2017-04-30 14:50:36 +0530

    Update

--- msit/urls.py
+++ msit/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.utils.text import ugettext_lazy as _
 
 
 urlpatterns = [
@@ -13,3 +14,9 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+admin.site.site_header = _('Maharaja Surajmal Institute of technology')
+admin.site.index_title = _('MSIT Administration')
+admin.site.site_title = _('MSIT Administration')
