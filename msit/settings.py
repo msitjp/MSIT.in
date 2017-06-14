78e4c25db9db7dabad06e25e9622957b69122db1
Hiten Sharma <sharmahiten3@gmail.com>
2017-06-14 20:47:34 +0530

    env added

--- msit/settings.py
+++ msit/settings.py
@@ -1,8 +1,11 @@
 import os
 
+import dotenv
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv.read_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
