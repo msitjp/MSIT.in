243bc5f990eed2995b14ebd6e515dcdc7909c0e6
Aditya <aditya.icf@gmail.com>
2019-07-29 19:26:02 +0530

    a

--- storage/admin.py
+++ storage/admin.py
@@ -429,7 +429,8 @@ class BookRecordAdmin(admin.ModelAdmin):
         if request.user.is_superuser or u.department == 'All':
             return qs
         else:
-            return qs.filter(faculty__department=u.department, faculty__shift=u.shift)
+#            return qs.filter(faculty__department=u.department, faculty__shift=u.shift)
+             return qs.filter(faculty__full_name=request.user.username.replace('-',' ').title())
 
   def get_list_filter(self, request):
     if request.user.userdepartment.department == 'All':
