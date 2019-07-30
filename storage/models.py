5a0aeb617a6474bca1945f9370fd3a3a37e74134
Aditya <aditya.icf@gmail.com>
2019-07-30 19:36:52 +0530

    price removed

--- storage/models.py
+++ storage/models.py
@@ -42,7 +42,6 @@ class BookRecord(models.Model):
   address = models.CharField(verbose_name="Address", max_length=200, null=True)
   isbn = models.CharField(verbose_name="ISBN", max_length=50, blank=False, null=True, validators=[RegexValidator('^[0-9-xX]*$')])
   pages = models.CharField(verbose_name="Total Pages", max_length=10, blank=False, null=True, validators=[RegexValidator('^[0-9]*$')])
-  price = models.CharField(verbose_name="Price", max_length=10, null=True)
   year = models.CharField(max_length=4, blank=False, validators=[RegexValidator('^[0-9]*$')])
   created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
   updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
