efc1a3a0588353c02fb1f41326118b12feb426e2
Aditya <aditya.icf@gmail.com>
2019-07-30 19:32:20 +0530

    Book

--- storage/models.py
+++ storage/models.py
@@ -42,7 +42,7 @@ class BookRecord(models.Model):
   address = models.CharField(verbose_name="Address", max_length=200, null=True)
   isbn = models.CharField(verbose_name="ISBN", max_length=50, blank=False, null=True, validators=[RegexValidator('^[0-9-xX]*$',code='regex')])
   pages = models.CharField(verbose_name="Total Pages", max_length=10, blank=False, null=True, validators=[RegexValidator('^[0-9]*$',code='regex')])
-  price = models.CharField(verbose_name="Price", max_length=10, null=True, validators=[RegexValidator('^[0-9]*$',code='regex')])
+  price = models.CharField(verbose_name="Price", max_length=10, null=True)
   year = models.CharField(max_length=4, blank=False, validators=[RegexValidator('^[0-9]*$',code='regex')])
   created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
   updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
