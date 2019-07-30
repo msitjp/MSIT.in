b039826cbf8545a27f735078c667db33102f76f1
Aditya <aditya.icf@gmail.com>
2019-07-30 19:19:38 +0530

    Book records

--- storage/models.py
+++ storage/models.py
@@ -31,7 +31,6 @@ INDEXING_TYPE = (
   ('Others', 'Others'),
 )
 
-
 class BookRecord(models.Model):
   title = models.CharField(verbose_name="Title/Topic", max_length=300, blank=False)
   faculty = models.ForeignKey(Faculty)
@@ -39,10 +38,12 @@ class BookRecord(models.Model):
                            max_length=3, blank=True, null=True)
   type = models.CharField(verbose_name="International/National", max_length=15, blank=False, choices=NATION, default=NATION[0][0])
   publisher = models.CharField(
-      verbose_name="Publisher", max_length=200, blank=False, null=True)
-  isbn = models.CharField(verbose_name="ISBN", max_length=50, blank=False, null=True)
-  pages = models.CharField(verbose_name="Total Pages", max_length=10, blank=False, null=True)
-  year = models.CharField(max_length=4, blank=False)
+      verbose_name="Publisher", max_length=200, blank=False, null=True, validators=[RegexValidator('^[a-zA-Z ,-]*$')])
+  address = models.CharField(verbose_name="Address", max_length=200, null=True)
+  isbn = models.CharField(verbose_name="ISBN", max_length=50, blank=False, null=True, validators=[RegexValidator('^[0-9-xX]*$',code='regex')])
+  pages = models.CharField(verbose_name="Total Pages", max_length=10, blank=False, null=True, validators=[RegexValidator('^[0-9]*$',code='regex')])
+  price = models.CharField(verbose_name="Price", max_length=10, null=True, validators=[RegexValidator('^[0-9]*$',code='regex')])
+  year = models.CharField(max_length=4, blank=False, validators=[RegexValidator('^[0-9]*$',code='regex')])
   created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
   updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
 
