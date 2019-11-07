9cb6917e6ab8421f5d3c4ee2a69635678fb890f9
Aditya <aditya.icf@gmail.com>
2019-11-08 01:13:23 +0530

    .

--- storage/admin.py
+++ storage/admin.py
@@ -308,7 +308,7 @@ def exportFDP(request, queryset=None):
           '-faculty__designation').values_list('faculty')))
     else:
 #      records = list(set(e[0] for e in FDPRecord.objects.filter(faculty__department=department).order_by('-faculty__designation').values_list('faculty')))
-      if not any(c.islower() for c in request.user.username):
+      if request.user.username=='CSEE':
           records = list(set(e[0] for e in ResearchRecord.objects.filter(faculty__department=department).order_by('-faculty__designation').values_list('faculty')))
       else:
           records = list(set(e[0] for e in BookRecord.objects.filter(faculty__full_name=request.user.username.replace('-',' ').title()).order_by('-faculty__designation').values_list('faculty')))
