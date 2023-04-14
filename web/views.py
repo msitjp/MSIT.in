7a6cdf91c379101ce65f4619d15fd315650d5114
Tanay Aggarwal <aggarwalnaman54@gmail.com>
2023-04-14 10:48:24 +0530

    NBA Accredited removed from CSE/IT

--- web/views.py
+++ web/views.py
@@ -192,7 +192,7 @@ def cse(request, sort=-1, order=-1):
     context.update(get_faculties(department, modifier))
     context['settings'] = department
     context['tabs'] = tabs
-    context['heading'] = 'Computer Science and Engineering (NBA Accredited)'
+    context['heading'] = 'Computer Science and Engineering'
     return render(request, 'faculty.html', context=context)
 
 
@@ -208,7 +208,7 @@ def it(request, sort=-1, order=-1):
     context.update(get_faculties(department, modifier))
     context['settings'] = department
     context['tabs'] = tabs
-    context['heading'] = 'Information technology (NBA Accredited)'
+    context['heading'] = 'Information technology'
     return render(request, 'faculty.html', context=context)
 
 
